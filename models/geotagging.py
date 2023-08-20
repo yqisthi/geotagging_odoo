40ab7b2fbfe271c29f72a1b85730daddc40b3f12
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-08-20 22:48:47 +0700

    old version

--- models/geotagging.py
+++ models/geotagging.py
@@ -9,27 +9,3 @@ class Geotagging(models.Model):
 
     latitude = fields.Float("latitude")
     longitude = fields.Float("longitude")
-
-    class GeolocationController(http.Controller):
-        @http.route('/web_geolocation', type='http', auth='user', website=True)
-        def web_geolocation(self, **kwargs):
-            return """
-                <script type="text/javascript">
-                    $(document).ready(function() {
-                        if ("geolocation" in navigator) {
-                            navigator.geolocation.getCurrentPosition(function(position) {
-                                var latitude = position.coords.latitude;
-                                var longitude = position.coords.longitude;
-
-                                $('input[name="latitude"]').val(latitude);
-                                $('input[name="longitude"]').val(longitude);
-
-                                console.log("latitude");
-                                console.log(longitude);
-                            });
-                        } else {
-                            console.log("Geolocation is not available.");
-                        }
-                    });
-                </script>
-            """
