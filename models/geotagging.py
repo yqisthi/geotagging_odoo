ac753e8eb83e0cc7e364a99bc6b7f0d52b76153e
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-08-20 22:27:39 +0700

    httprouting commit

--- models/geotagging.py
+++ models/geotagging.py
@@ -1,9 +1,35 @@
 from odoo import api, fields, models, _
+from odoo import http
+from odoo.http import request
 
 class Geotagging(models.Model):
     _name = "geotagging"
     _inherit = []
     _description = "Geotagging"
 
-    latitude = fields.Char("latitude")
-    longitude = fields.Char("longitude")
+    latitude = fields.Float("latitude")
+    longitude = fields.Float("longitude")
+
+    class GeolocationController(http.Controller):
+        @http.route('/web_geolocation', type='http', auth='user', website=True)
+        def web_geolocation(self, **kwargs):
+            return """
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        if ("geolocation" in navigator) {
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                                var latitude = position.coords.latitude;
+                                var longitude = position.coords.longitude;
+
+                                $('input[name="latitude"]').val(latitude);
+                                $('input[name="longitude"]').val(longitude);
+
+                                console.log("latitude");
+                                console.log(longitude);
+                            });
+                        } else {
+                            console.log("Geolocation is not available.");
+                        }
+                    });
+                </script>
+            """
