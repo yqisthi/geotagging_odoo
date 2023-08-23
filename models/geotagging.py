cb54a78d7cf8e477cab113238dbd50038e4291a4
RazoelZ <danydarmawannn@gmail.com>
2023-08-24 02:45:36 +0700

    sebelum error menyerang

--- models/geotagging.py
+++ models/geotagging.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models, _
-import requests
 from odoo.exceptions import UserError, ValidationError
+import requests
+import socket
 
 
 class Geotagging(models.Model):
@@ -27,18 +28,28 @@ class Geotagging(models.Model):
 
     # this metode is works!
     def action_get_coordinates_by_ip(self):
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
         for partner in self:
             ip_address = partner.ip_address_field  # Replace with the actual field name
             coordinates = partner.get_coordinates_by_ip(ip_address)
             partner.latitude = coordinates['latitude']
             partner.longitude = coordinates['longitude']
-            raise ValidationError(_("Latitude: %s, Longitude: %s") % (
-                partner.latitude, partner.longitude))
+            partner.ip_address_field = ip_address
+            raise ValidationError(_("Latitude: %s, Longitude: %s, IP Address: %s") % (
+                partner.latitude, partner.longitude, partner.ip_address_field))
 
     # trigger when ip_address_field is changed
-    @api.onchange('ip_address_field')
+    # @api.onchange('ip_address_field')
     def fill_coordinates_on_ip_change(self):
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
         if self.ip_address_field:
             coordinates = self.get_coordinates_by_ip(self.ip_address_field)
+            self.ip_address_field = ip_address
             self.latitude = coordinates['latitude']
             self.longitude = coordinates['longitude']
+
+    # search ip address
+    def get_ip_address(self):
+        raise ValidationError(_("IP Address: %s") % (ip_address))
