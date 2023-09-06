f4f80c295c28d8c4048fd29c414a24f3b946e10b
RazoelZ <danydarmawannn@gmail.com>
2023-09-07 02:48:02 +0700

    adding newest feature

--- models/geotagging.py
+++ models/geotagging.py
@@ -16,18 +16,16 @@ class Geotagging(models.Model):
 
     isreadonly = fields.Boolean(String="is readonly?", compute='compare')
 
+    isnear = fields.Selection([
+        ('far', 'Far'),
+        ('medium', 'Medium'),
+        ('near', 'Near')
+    ], string="How Far from Office?", compute='_compute_isnear', store=True)
+
     lat = fields.Float(String="latitude", digits=(
         16, 5), related='location_id.latitude')
     lot = fields.Float(String="longitude", digits=(
         16, 5), related='location_id.longitude')
-
-    def comparison(self):
-        if self.latitude > self.lat:
-            raise ValidationError(
-                "Latitude asli lebih besar dari pada latitude palsu")
-        else:
-            raise ValidationError(
-                "Latitude asli lebih kecil dari pada latitude palsu")
 
     @api.depends('lat')
     def compare(self):
@@ -35,3 +33,23 @@ class Geotagging(models.Model):
             self.isreadonly = True
         else:
             self.isreadonly = False
+
+    @api.depends('latitude', 'longitude', 'location_id.latitude', 'location_id.longitude')
+    def _compute_isnear(self):
+        for record in self:
+            latitude_diff = abs(record.lat - record.latitude)
+            longitude_diff = abs(record.lot - record.longitude)
+
+            if latitude_diff <= 10 and longitude_diff <= 10:
+                record.isnear = 'near'
+            elif 10 < latitude_diff <= 50 or 10 < longitude_diff <= 50:
+                record.isnear = 'medium'
+            elif latitude_diff > 50 or longitude_diff > 50:
+                record.isnear = 'far'
+
+    def comparing(self):
+        latitude_diff = abs(self.lat - self.latitude)
+        longitude_diff = abs(self.lot - self.longitude)
+        error_message = f"latitude: {self.latitude}, longitude: {self.longitude}, lat: {self.lat}, lot: {self.lot}, latitude diff: {latitude_diff}, longitude diff : {longitude_diff}"
+
+        raise ValidationError(error_message)
