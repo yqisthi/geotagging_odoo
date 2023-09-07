ac741eaa7fa86a5dcd25c9c001b89a7c2d67f378
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-09-07 13:38:49 +0700

    add comment

--- static/js/geolocation.js
+++ static/js/geolocation.js
@@ -1,17 +1,23 @@
 odoo.define("geotagging_odoo.geolocation", function (require) {
   "use strict";
-  console.log("LOADED asdf");
+  // Print out console if javascript file works
+  console.log("LOADED");
 
+  // Get Odoo View Controller and rpc
   var FormController = require("web.FormController");
   var rpc = require("web.rpc");
 
+  // Odoo View Controller
   FormController.include({
+    // Function executed on save
     saveRecord: function () {
       console.log("Saved!");
       var self = this;
       return this._super.apply(this, arguments).then(function () {
+        // Get Current Record
         var recordID = self.model.get(self.handle, { raw: true }).res_id;
 
+        // If record obtained, browser ask user for coordinates using GPS
         if (recordID) {
           navigator.geolocation.getCurrentPosition(function (position) {
             const { latitude, longitude } = position.coords;
@@ -19,10 +25,10 @@ odoo.define("geotagging_odoo.geolocation", function (require) {
               latitude,
               longitude,
             };
-            console.log(coords);
+            // Write record
             rpc
               .query({
-                model: "geotagging", // Replace with your actual model name
+                model: "geotagging",
                 method: "write",
                 args: [
                   [recordID],
@@ -44,32 +50,3 @@ odoo.define("geotagging_odoo.geolocation", function (require) {
 
   return FormController;
 });
-
-// var ExtendFormController = FormController.include({
-//   saveRecord: function () {
-//     var res = this._super.apply(this, arguments);
-//     self = this;
-//     if (this.modelName == "geotagging") {
-//       console.log("HELLOO");
-//       navigator.geolocation.getCurrentPosition(function (position) {
-//         const { latitude, longitude } = position.coords;
-//         const coords = {
-//           latitude,
-//           longitude,
-//         };
-//         console.log(coords);
-//         self
-//           ._rpc({
-//             model: "geotagging",
-//             method: "get_location",
-//             args: [[], coords],
-//           })
-//           .then(function (result) {
-//             // window.location.reload();
-//           });
-//       });
-//     }
-
-//     return res;
-//   },
-// });
